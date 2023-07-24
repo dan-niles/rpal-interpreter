725ac51b0cf5a4613771a55a4dab4bc061e071db
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-07-24 17:36:26 +0530

    Update environment

--- environment.h
+++ environment.h
@@ -10,17 +10,12 @@ class environment
 public:
 	environment *prev;
 	string name;
-	// tree* key;
-	// tree* value;
 	map<tree *, vector<tree *> > boundVar;
 
 	environment()
 	{
 		prev = NULL;
 		name = "env0";
-		// key = NULL;
-		// value = NULL;
-		// boundVar
 	}
 
 	// Copy constructor
